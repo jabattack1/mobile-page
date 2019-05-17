e443260dfd59edbcc2630d0a56e9036821042d29
jabattack1 <peter.hahn@live.com>
2019-05-16 21:47:47 -0400

    liquid

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ChatContainer from './chat/chat-container.react';
 import HotelController from './hotel/hotel-controller.react';
 
 ReactDOM.render(
